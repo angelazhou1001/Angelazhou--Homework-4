9ea2bcfcad54c314913bb93a50f0fc9724061c69
qiuchizhou <qiuchizhou@gmail.com>
2018-08-06 22:26:40 -0400

    Assgiment4

---
+++ step2.py
@@ -0,0 +1,6 @@
+myfavmovies = {'The Pursuit of Happiness','How to be Single','50 Shades of Grey'}
+hisfavmovies = {'The Shawshank Redemption','The Pursuit of Happiness','Jin Ping Mei'}
+commonset = myfavmovies.intersection(hisfavmovies)
+print (commonset)
+myset = myfavmovies.difference(hisfavmovies)
+print(myset)
---
+++ step3.py
@@ -0,0 +1,2 @@
+dailytask = ('woke up','eat breakfast','dress up','go to work','work in the office','lunch','work again','get off work','go shopping','eat dinner','browse the internet','go to bed')
+print("This is how my day goes", dailytask)
